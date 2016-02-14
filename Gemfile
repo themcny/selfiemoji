6d9170a69c7dac5db8de4ff2fd36bd3c2841aeb9
Daniel Woznicki <tokay89@sbcglobal.net>
2016-02-14 11:30:55 -0800

    Add imgur api, allow emotion api to access last image

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'headshot'
 gem 'dotenv'
 gem 'twemoji'
 gem 'httparty'
+gem 'imgurapi'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
+require 'net/http'
 include EmojiHelper
+
 class PicturesController < ApplicationController
   helper :headshot
   def index
@@ -7,8 +9,13 @@ class PicturesController < ApplicationController
 
   def get_emotion
   	p "0" * 50
-    uri = URI('https://api.projectoxford.ai/emotion/v1.0/recognize')
+  	imgur_session = Imgurapi::Session.new(client_id: ENV['IMGUR_CLIENT_ID'], client_secret: ENV['IMGUR_CLIENT_SECRET'], access_token: ENV['IMGUR_ACCESS_TOKEN'], refresh_token: ENV['IMGUR_REFRESH_SECRET'])
+  	
+  	account = imgur_session.account.account
+  	image = imgur_session.image.image_upload("public/#{params[:image]}")
+  	p image.link
 
+    uri = URI('https://api.projectoxford.ai/emotion/v1.0/recognize')
     request = Net::HTTP::Post.new(uri.request_uri)
     # Request headers
     request['Content-Type'] = 'application/json'
@@ -16,7 +23,8 @@ class PicturesController < ApplicationController
     request['Ocp-Apim-Subscription-Key'] = ENV['PRIMARY_KEY']
     # Request body
     
-    request.body = {url: }.to_json
+    request.body = {url: image.link}.to_json
+    puts request.body
 
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
         http.request(request)
@@ -24,5 +32,7 @@ class PicturesController < ApplicationController
 
     p "*" * 50
     p response.body
+
+    redirect_to root_path
   end
 end

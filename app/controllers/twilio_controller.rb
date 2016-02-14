19eaed1992a44a9e627aaedde527681bb88d4030
manisaguana <armanisaldana@gmail.com>
2016-02-14 11:56:50 -0800

    finish twilio controller?

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,19 +1,25 @@
 require 'twilio-ruby' 
- 
-# put your own credentials here 
-# Required for any Twilio Access Token
-account_sid = 'AC782792d9197866bebda2b141ac46a273k' 
-api_key = 'SKd3516f4379a9b849305954900053465e'
-api_secret = ENV['API_SECRET']
 
-auth_token = '[AuthToken]' 
- 
-# set up a client to talk to the Twilio REST API 
-@client = Twilio::REST::Client.new account_sid, auth_token 
- 
-@client.account.messages.create({
-  :from => '+16507302789', 
-  :to => '+14154243199', 
-  :body => 'Hey Armani! I hope you like my pics!', 
-  :media_url => 'http://pof.com/1075/bae.jpg', 
-})
+class TwilioController < ApplicationController
+	def  post 
+	  # put your own credentials here 
+		# Required for any Twilio Access Token
+		account_sid = ENV['ACCOUNT_SID'] 
+		api_key = ENV['API_KEY']
+		api_secret = ENV['API_SECRET']
+
+		auth_token = ENV['AUTH_TOKEN']
+		 
+		# set up a client to talk to the Twilio REST API 
+		@client = Twilio::REST::Client.new account_sid, auth_token 
+		 
+		@client.account.messages.create({
+		  :from => '+16507302789', 
+		  :to => '+14154243199', 
+		  :body => 'Hey Armani! I hope you like my pics!', 
+		  # :media_url => 'http://pof.com/1075/bae.jpg', 
+		})
+	end	
+end
+
+

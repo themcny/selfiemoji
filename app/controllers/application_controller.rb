87d6d799aa551881a920dd744840d7322ceb99c1
Daniel Woznicki <tokay89@sbcglobal.net>
2016-02-13 17:16:44 -0800

    Add after save method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  def headshot_post_save(file_path)
+  	p "*" * 50
+		p file_path
+	end
 end

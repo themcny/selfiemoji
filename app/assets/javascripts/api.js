6160fffbc8918557fa04986f68feb942c14617be
Jeremy <jp5486@gmail.com>
2016-02-14 16:10:33 -0800

    resolve emoji positioning

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,48 +1,18 @@
 $(document).ready(function(){
 //call functions
-  // hideCamera();
+  emojify();
 });
-function hideCamera(){
-  $(document).on("click", "#shoot-button", function(e){
-    $('#camera-container').addClass('hidden');
+
+function emojify(){
+  $(document).on('click', "#emojify", function(e){
+    e.preventDefault();
+    var imgSrc = $(this).parent().next().attr('src')
     $.ajax({
-      url: "/pictures/last_picture",
-      type: "GET"
+      url: '/pictures/get_emotion',
+      type: 'POST',
+      data: {image: imgSrc}
+    }).done(function(response){
+      console.log("success!!!!! :$")
     })
-    .done(function(response) {
-      $('#latest-picture-container').append(
-        "<img src=headshots/" + response.photo + ">"
-      )
-    })
-    .fail(function() {
-      console.log("Failure")
-    })
-    $('#latest-picture-container').removeClass('hidden');
   })
 }
-
-//
-//
-// function getEmotionData(){
-//   $(document).on('click', "#get_emotion", function(e){
-//     e.preventDefault();
-//     $.ajax({
-//         url: "https://api.projectoxford.ai/emotion/v1.0/recognize?" + $.param(params),
-//           beforeSend: function(xhrObj){
-//               // Request headers
-//               xhrObj.setRequestHeader("Content-Type","application/json");
-//               xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{subscription key}");
-//           },
-//           type: "POST",
-//           // Request body
-//           data: "{body}",
-//       })
-//       .done(function(data) {
-//           alert("success");
-//       })
-//       .fail(function() {
-//           alert("error");
-//       });
-//     });
-//   })
-// }
